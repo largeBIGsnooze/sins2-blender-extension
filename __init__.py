fae075b14cec845960a1d58e774087ccbe733b0c
Tyloth <miguelbernatskyy3@gmail.com>
2025-01-15 11:58:05 +0000

    increment version number

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Sins II Extension",
     "description": "Extension for importing and exporting Sins of a Solar Empire 2 meshes whilst leveraging official tooling",
     "author": "Tyloth, Cyno Studios",
-    "version": (0, 9, 8),
+    "version": (0, 9, 9),
     "blender": (4, 1, 0),
     "location": "3D View",
     "category": "Import-Export",
